c284fe18a3331490747692a66c61fdfa884590d2
Mimi _kuraga <python_data_processing@python-data-processings-macbook-air.local>
2018-03-30 20:49:49 +0300

    Initial Commit

---
+++ dicee2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dicee2
+//
+//  Created by Python_data_processing on 30/03/2018.
+//  Copyright © 2018 Digital Disruptive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
